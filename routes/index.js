7cc6301f9e5f3fd7495dd94b2ec825d1478929f9
Scott Hodges <shodges201@gmail.com>
2019-10-09 18:34:16 -0400

    fixing route issues not serving up any content to start when app is deployed

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 
 // API Routes
-router.use("/", apiRoutes);
+router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  console.log('/');
+  console.log('/api');
   console.log(path.join(__dirname, "../client/build/index.html"));
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
