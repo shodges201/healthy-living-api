61982c350354459b49be093cb07b3db85637ca6f
Scott Hodges <shodges201@gmail.com>
2019-10-09 18:57:39 -0400

    fixing no content sent error

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,8 +5,8 @@ const heartRate = require("./heartRate/heartRate");
 // const firebase = require('firebase-admin');
 
 // API Routes
-router.use("/api/cholesterol/", apiRoutes);
-router.use("/api/heartrate/", heartRate);
+router.use("/cholesterol/", apiRoutes);
+router.use("/heartrate/", heartRate);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const app = express();
 
 const PORT = process.env.PORT || 9000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
