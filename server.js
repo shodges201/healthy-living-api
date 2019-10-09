f7e2bf6f329888e03b050b3a2074b8995e86c597
Scott Hodges <shodges201@gmail.com>
2019-10-09 17:51:33 -0400

    refactored paths for deployment

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(routes);
 // Serve up static assets (usually on heroku)
 console.log(path.join(__dirname, 'build', 'index.html'));
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, 'build', 'index.html')));
+  app.use(express.static(path.join(__dirname, 'build')));
 }
 
 admin.initializeApp({
