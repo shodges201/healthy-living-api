7eee37e0f90cb65db2b20be4c5dc74d589a40928
Scott Hodges <shodges201@gmail.com>
2020-02-25 23:01:19 -0500

    changed CompleteButton to have submit type so user can hit enter to submit login or signup credentials

--- client/src/Components/CompleteButton/CompleteButton.tsx
+++ client/src/Components/CompleteButton/CompleteButton.tsx
@@ -4,7 +4,7 @@ import Button from '@material-ui/core/Button';
 function CompleteButton(props: {text: string, class: string, size:"small" | "medium" | "large" | undefined, handleForm: Function}) {
     console.log(props.class);
     return (
-        <Button onClick={(event) => props.handleForm(event)} variant="contained" size={props.size} color="primary" className={props.class}>
+        <Button onClick={(event) => props.handleForm(event)} variant="contained" size={props.size} color="primary" className={props.class} type="submit">
             {props.text}
         </Button>
     )
