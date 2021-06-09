90a6d9f3a6f25365098b5bcec540bfbaf2c83a97
Scott Hodges <shodges201@gmail.com>
2021-06-08 21:59:33 -0400

    removed client from project to seperate concerns and make microservice instead

--- client/src/Components/NoData/NoData.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import "./NoData.css";
-export default function NoData() {
-    return (React.createElement("div", { className: "no-data-container" },
-        React.createElement("h1", { className: "no-data-text" }, "You haven't entered any data yet! Use the button below to enter some information and after your first entry a graph will appear.")));
-}
-//# sourceMappingURL=NoData.js.map
